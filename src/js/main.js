0845746de3e4dee3f42cc449471c6069c12002c8
Sergey Khmelevskoy <xs290@me.com>
2017-09-22 18:11:43 +0300

    particles

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,13 @@
 $(document).ready(function(){
 
-  //////////
-  // Global variables
-  //////////
-
-  var _window = $(window);
-  var _document = $(document);
-
-  function isRetinaDisplay() {
-    if (window.matchMedia) {
-        var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
-        return (mq && mq.matches || (window.devicePixelRatio > 1));
-    }
-  }
-
-  var _mobileDevice = isMobile();
-  // detect mobile devices
-  function isMobile(){
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  //////////
-  // COMMON
-  //////////
-
  	// Prevent # behavior
 	$('[href="#"]').click(function(e) {
 		e.preventDefault();
 	});
+
+  var scene = $('.js-scene').get(0)
+
+  var parallax = new Parallax(scene);
 
 });
 
