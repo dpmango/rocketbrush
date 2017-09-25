a354fcc838a29016a176db207f86c1ee60ee998d
dpmango <xs290@me.com>
2017-09-25 16:03:07 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -10,21 +10,16 @@ $(document).ready(function(){
   var parallax = new Parallax(scene);
 
   $('.footer-face').hover(function(){
-    // on
     toggleFaceDesc( $(this).data('id') )
   }, function(){
-    // out
     toggleFaceDesc( )
   })
 
   function toggleFaceDesc(el){
-    console.log(el)
     $('.footer-face_desc').removeClass('is-active')
-    // if id is provided
     if ( el ){
       $('.footer-face_desc[data-id='+el+']').addClass('is-active')
     }
-
   }
 });
 
